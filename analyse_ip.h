cdf3f33c65f7ad44cca4718766e9948173a405ec
Amine.M <147102714+aminemrb@users.noreply.github.com>
2024-12-13 10:20:32 +0100

    Update analyse_ip.h

--- analyse_ip.h
+++ analyse_ip.h
@@ -8,7 +8,7 @@ int verifier_format(const char *ip_avec_masque);
 int extraire_champs(const char *ip_avec_masque, char *ip, int *masque);
 
 // Fonction pour décoder l'adresse IP et déterminer sa classe et son type
-void decoder_ip(const char *ip, char *classe_ip, char *type_ip, int masque);
+void decoder_ip(const char *ip, char *classe_ip, char *type_ip);
 
 // Fonction pour convertir l'adresse IP en valeurs numériques
 void convertir_en_numerique(const char *ip, int ip_numerique[4]);
@@ -18,5 +18,8 @@ void calculer_reseau_hote(const char *ip, int masque, int *reseau, int *hote);
 
 // Fonction pour afficher l'adresse IP en format pointé
 void afficher_ip(unsigned long ip);
+
+//Fonction pour obtenir le masque de sous réseau
+void convertir_masque(int masque_cidr, unsigned char masque_dec[4]);
 
 #endif /* ANALYSE_IP_H */
